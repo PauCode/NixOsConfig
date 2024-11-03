163061f02a24a1ffe6822189d107b8fa1e5c7b4e
Paul Furtuna <onwebbass@gmail.com>
2024-11-03 21:59:02 +0200

    xrandr setup saved

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -9,6 +9,7 @@
       ll = "eza -alh";
       norse = "sudo nixos-rebuild switch --flake /etc/nixos#THUNDER";
       horse = "home-manager switch --flake /etc/nixos#paulzstorm@THUNDER";
+      "mm-setup" = "xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --output DP-4 --mode 1920x1080 --pos 1920x0 --output DP-2 --mode 1920x1080 --pos 3840x0";
     };
     history = {
       size = 10000;
