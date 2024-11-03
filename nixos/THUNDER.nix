a9f1d5c618e961c87ee7227f793d620dff309f9f
PauCode <62845917+PauCode@users.noreply.github.com>
2024-11-03 19:23:22 +0200

    Create THUNDER.nix

---
+++ nixos/THUNDER.nix
@@ -0,0 +1,30 @@
+{
+  imports = [
+    # base config with pkgs and services
+    ./modules/base.nix
+    ./users/paulzstorm.nix
+
+    # modules
+    ./modules/pulseaudio.nix
+    ./modules/bluetooth.nix
+    ./modules/cups.nix
+    ./modules/gaming.nix
+    ./modules/appimage.nix
+
+    # hardware
+    ./hardware/machines/THUNDER.nix
+    ./hardware/nvidia.nix
+
+    # graphics
+    ./graphics/awesomewm.nix
+
+    # services
+    ./services/flatpak.nix
+    ./services/transmission.nix
+    ./services/gnome-settings.nix
+  ];
+
+  networking.hostName = "THUNDER";
+
+  services.transmission.home = "/bigboy/Torrents";
+}
