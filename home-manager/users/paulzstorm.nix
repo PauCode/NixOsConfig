c4c4cbb7a95f3181d5f6506c84e411f883027cdb
PauCode <62845917+PauCode@users.noreply.github.com>
2024-11-03 18:31:25 +0200

    Create paulzstorm.nix

---
+++ home-manager/users/paulzstorm.nix
@@ -0,0 +1,18 @@
+{
+  home = {
+    username = "paulzstorm";
+    homeDirectory = "/home/paulzstorm";
+    sessionVariables = {
+      EDITOR = "neovim";
+      TERMINAL = "alacritty";
+    };
+  };
+
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
+}
