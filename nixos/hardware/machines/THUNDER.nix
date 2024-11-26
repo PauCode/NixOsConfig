2e99186777409abbe1fcf53e2768f9957d7659a1
CloudyChris <a.c.pufu@gmail.com>
2024-11-26 22:53:28 +0200

    Gaming

--- nixos/hardware/machines/THUNDER.nix
+++ nixos/hardware/machines/THUNDER.nix
@@ -128,5 +128,6 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.enableAllFirmware = true;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
